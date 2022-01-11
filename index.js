b3276fdb55d575cc025fde379da790de2136cbd9
Amanda <amandamaiken@berkeley.edu>
2022-01-11 09:37:04 -0800

    commit tree c62adbbc89927e60fb78a64c78eab0077f05f2a4

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+Math.floor(Math.random() > 0) + 1;
+
+const num3 = 16;
+const num4 = 4;
+
+const mod = num3 / num4;
+
+Math.max(18, 19, 20); 
